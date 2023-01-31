3875802efe3a81181d05f70bd04a088ce4575132
Nonexistent-Name <58897346+Nonexistent-Name@users.noreply.github.com>
2023-01-31 13:30:56 -0800

    Minor asthetic improvements

--- viewer.js
+++ viewer.js
@@ -1,6 +1,4 @@
-const controls = document.getElementById('connectControls');
-const peerInput = document.getElementById('peerInput');
-const connectBtn = document.getElementById('connectBtn');
+const overlay = document.getElementById('overlay');
 const streamDisplay = document.getElementById('stream');
 
 const peer = new Peer();
@@ -8,38 +6,79 @@ const peer = new Peer();
 peer.on('call', call => {
     call.answer();
 
-    console.log('Video connection established');
+    console.log('3/4 Stream established');
 
     call.on('stream', stream => {
-        console.log('Stream recieved');
+        console.log('4/4 Stream recieved');
 
-        controls.style.display = 'none';
+        overlay.style.display = 'none';
         streamDisplay.srcObject = stream;
         streamDisplay.style.display = 'unset';
     });
 
     call.on('close', () => {
-        controls.style.display = '';
+        overlay.style.display = '';
         streamDisplay.srcObject = null;
     });
 });
 
+const connectBtn = document.getElementById('connectBtn');
+
+let connection;
+
 connectBtn.addEventListener('click', () => {
-    console.log(`Connecting to ${peerInput.value}`);
+    connection = peer.connect(peerInput.value);
 
-    const conn = peer.connect(peerInput.value);
+    connectBtn.innerText = 'Connecting...';
+    connectBtn.style.backgroundColor = 'white';
+    connectBtn.style.color = 'black';
+    connectBtn.disabled = true;
+    console.log(`1/4 Connecting to ${peerInput.value}`);
+
     conn.on('open', () => {
-        console.log('Input connection established');
+        console.log('2/4 Connection established');
+    });
 
-        addEventListener('keyup', event => {
-            conn.send({
-                type: 'key',
-                key: event.key.toLowerCase(),
-            });
-        });
+    conn.on('close', () => {
+        console.log('Connection closed');
+    });
+
+    conn.on('error', err => {
+        connections = connections.filter(c => c !== conn);
+
+        console.error(err);
     });
 });
 
-document.body.addEventListener('click', () =>
-    document.body.requestFullscreen()
-);
+const peerInput = document.getElementById('peerInput');
+
+peerInput.addEventListener('blur', () => {
+    if (peerInput.style.display !== 'none') peerInput.focus();
+});
+
+peerInput.addEventListener('keyup', event => {
+    if (event.key === 'Enter') connectBtn.click();
+});
+
+const keyEvent = event => {
+    connections.forEach(conn => {
+        conn.send({
+            type: 'key',
+            key: event.key.toLowerCase(),
+            state: event.type,
+        });
+    });
+};
+
+addEventListener('keydown', keyEvent);
+addEventListener('keyup', keyEvent);
+
+document.body.addEventListener('click', () => {
+    if (document.fullscreenElement || !streamDisplay.srcObject) return;
+
+    try {
+        document.body.requestFullscreen();
+    } catch (err) {
+        console.error(`Fullscreen request failed: ${err}`);
+    }
+});
